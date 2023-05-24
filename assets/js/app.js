bd5a00279ba5ff7bdd142018c024862a5d62e38f
Rajeshsutare <sutarerns@gmail.com>
2023-05-24 11:48:54 +0530

    2nd commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,13 +5,13 @@ const eyeIcon = document.getElementById("eyeIcon")
 const password = document.getElementById("password")
 
 const onPassword = (eve) =>{
-    cl("clicked")
+    cl("clicked");
     if(password.type === 'password'){
         password.type = 'text' ;
-        eyeIcon.classList.replace('fa-eye-slash','fa-eye')
+        eyeIcon.classList.replace('fa-eye-slash','fa-eye');
     }else{
-        password.type = 'password'
-        eyeIcon.classList.replace('fa-eye','fa-eye-slash')
+        password.type = 'password';
+        eyeIcon.classList.replace('fa-eye','fa-eye-slash');
 
     }
 }
