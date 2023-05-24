cc7909dae93dd894956c3334a31ad44754a55700
Rajeshsutare <sutarerns@gmail.com>
2023-05-24 10:52:02 +0530

    passwod hide work

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+
+let cl = console.log;
+
+const eyeIcon = document.getElementById("eyeIcon")
+const password = document.getElementById("password")
+
+const onPassword = (eve) =>{
+    cl("clicked")
+    if(password.type === 'password'){
+        password.type = 'text' ;
+        eyeIcon.classList.replace('fa-eye-slash','fa-eye')
+    }else{
+        password.type === 'password'
+        eyeIcon.classList.replace('fa-eye','fa-eye-slash')
+
+    }
+}
+
+
+
+
+eyeIcon.addEventListener("click",onPassword)
